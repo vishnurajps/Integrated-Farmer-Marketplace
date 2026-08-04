863b8df018af204af0703bb399961249408f103b
VR <vishnurajps90@gmail.com>
2026-08-04 15:04:10 +0530

    Added Spring Boot backend setup

---
+++ backend/src/main/java/com/ifmap/IfmapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ifmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfmapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfmapBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ifmap/IfmapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfmapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
